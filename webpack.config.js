b83f6c51e91d0325053c269ca75c18c3e1adc94f
Sean O'Connor <seanoconnor941@gmail.com>
2021-02-23 13:22:14 -0700

    removeComments

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,9 @@ module.exports = {
       template: path.resolve(__dirname, "public/index.html"),
       hash: true,
       scriptLoading: "defer",
+      minify: {
+        removeComments: true,
+      },
     }),
   ],
 
