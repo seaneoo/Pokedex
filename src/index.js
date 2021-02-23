ea41313e6f13de92cf8afc8d61f5f2f422745ae1
Sean O'Connor <seanoconnor941@gmail.com>
2021-02-23 12:30:16 -0700

    App container

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
+import App from "~/containers/App";
 
-render(<></>, document.getElementById("app"));
+render(<App />, document.getElementById("app"));
